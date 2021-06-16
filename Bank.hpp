b5754371df9ade6f4e358f803e4cdd6602468f62
nschevokin <79716949+nschevokin@users.noreply.github.com>
2021-06-16 22:55:13 +0300

    Update Bank.hpp

--- Bank.hpp
+++ Bank.hpp
@@ -3,50 +3,93 @@
 
 #include "AccountList.hpp"
 
-//класс банка
+
+/**
+ * класс банка
+ */
 class Bank {
-    //поля
-    //код банка
+
     int code;
-    //список аккаунтов которые зарегистрированы в банке
+
     AccountList accounts;
 
 public:
-    //конструктор по умолчанию
+
     Bank();
 
-    //конструктор с параметром кода банка
+
+
     Bank(int code);
 
-    //метод создания нового аккаунта
+
+    /**
+     * метод создания нового аккаунта
+     * @param number
+     * @param surname
+     * @param name
+     * @param middleName
+     * @param balance
+     * @return
+     */
     Account createNewAccount(std::string number, std::string surname, std::string name, std::string middleName, double balance);
 
-    //метод добавления нового аккаунта
+
+    /**
+     * метод добавления нового аккаунта
+     * @param account
+     */
     void addAccount(const Account &account);
 
-    //удаление аккаунта
+
+    /**
+     * удаление аккаунта
+     * @param account
+     */
     void deleteAccount(const Account &account);
 
-    //проверка на существование
+
+    /**
+     * проверка на существование
+     * @param account
+     * @return true/false
+     */
     bool existsAccount(const Account &account);
 
-    //получение индекса аккаунта в списке
     int GetIndex(const Account &account);
 
-    //метод поиска аккаунта по номеру
+    /**
+     * метод поиска аккунта по номеру
+     * @param number
+     * @return возвращается ссылка на аккаунт
+     */
     Account *searchByNumber(std::string number);
 
-    //метод перевода денег между счетами по их номерам
+    /**
+     * метод перевода денег между счетами по их номерам
+     * @param senderNumber
+     * @param receiveNumber
+     * @param sum
+     */
     void transaction(std::string senderNumber, std::string receiveNumber, double sum);
 
     int getCode();
 
     bool operator==(const Bank &bank) const;
 
-    //оператор вывода
+    /**
+     * оператор вывода
+     * @param out
+     * @param bank
+     * @return out
+     */
     friend std::ostream &operator<<(std::ostream &out, const Bank &bank);
 
-    //оператор ввода
+    /**
+     * оператор ввода
+     * @param in
+     * @param bank
+     * @return in
+     */
     friend std::istream &operator>>(std::istream &in, Bank &bank);
 };
 
