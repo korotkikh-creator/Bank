1a6fc413ac74a0df7122eeb09a443ce8a2f98ae5
korotkikh-creator <78914008+korotkikh-creator@users.noreply.github.com>
2021-06-17 11:36:10 +0300

    Update AccountList.hpp

--- AccountList.hpp
+++ AccountList.hpp
@@ -13,57 +13,88 @@
  * Класс всех аккаунтов (список аккаунтов)
  */
 class AccountList {
-    //вектор - список
     std::vector<Account> accounts;
 
 public:
     /**
      * Метод добавления нового аккаунта в список всех аккаунтов
      * @param account
-     */
+     * @throws UserExistsException если указатель не указывает в пустоту при поиске searchByNumber
+    */
     void addUser(const Account &account);
 
     /**
      * Метод поиска аккаунта по его номеру
      * @param number
-     * @return
+     * @return указатель на аккаунт
      */
     Account *searchByNumber(std::string number);
 
     /**
      * Метод удаления аккаунта из списка всех аккаунтов
      * @param account
-     * @return
+     * @return true/false
+     * @throws EmptyListException если размер списка аккаунтов равен 0
      */
     bool deleteUser(const Account &account);
 
     /**
-     * Метод получения первого аккаунта из списка
-     * @return
+     * Для получения начального элемента в списке
+     * @return указатель this
+     * @throws EmptyListException если размер списка аккаунтов равен 0
      */
     Account &getFirstAccount();
 
     /**
-     * Метод получения последнего аккаунта из списка
-     * @return
+     * Для получения последнего элемента в списке
+     * @return указатель this
+     * @throws EmptyListException если размер списка аккаунтов равен 0
      */
     Account &getLastAccount();
 
-    //Перегрузка оператора индексации
+    /**
+     * Перегрузка оператора индексации
+     * @param index
+     * @return account[index]
+     * @throws IndexOfRangeException если индекс вышел за пределы индексации массива
+     */
     Account &operator[](int index);
 
-    //Метод проверки на существование аккаунта
+    /**
+     * Проверка на существование аккаунта в списке аккаунтов
+     * @param account
+     * @return true/false
+     */
     bool exists(const Account &account);
 
-    //Метод получения размера списка (количество аккаунтов)
+    /**
+     * Метод определения размера списка аккаунтов
+     * @return int accounts.size()
+     * @throws EmptyListException если размер списка аккаунтов равен 0
+     */
     int getSize() const;
 
+    /**
+     * Оператор сравнения для AccountList
+     * @param accountList
+     * @return true/false
+     */
     bool operator==(const AccountList &accountList) const;
 
-    //оператор вывода
+    /**
+     * ostream поток на вывод
+     * @param out
+     * @param accLst
+     * @return out
+     */
     friend std::ostream &operator<<(std::ostream &out, const AccountList &accLst);
 
-    //опратор ввода
+    /**
+     * istream поток на ввод
+     * @param in
+     * @param accLst
+     * @return in
+     */
     friend std::istream &operator>>(std::istream &in, AccountList &accLst);
 };
 
